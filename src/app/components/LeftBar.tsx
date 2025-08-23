0c0694fd058b04b0f9fa567a9aa031dda39147b9
samy884 <ssgill884@gmail.com>
2025-08-23 19:10:00 +0530

    Displaying menu left items

---
+++ src/app/components/LeftBar.tsx
@@ -0,0 +1,109 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const menuList = [
+  {
+    id: 1,
+    name: "Homepage",
+    link: "/",
+    icon: "home.svg",
+  },
+  {
+    id: 2,
+    name: "Explore",
+    link: "/",
+    icon: "explore.svg",
+  },
+  {
+    id: 3,
+    name: "Notification",
+    link: "/",
+    icon: "notification.svg",
+  },
+  {
+    id: 4,
+    name: "Messages",
+    link: "/",
+    icon: "message.svg",
+  },
+  {
+    id: 5,
+    name: "Bookmarks",
+    link: "/",
+    icon: "bookmark.svg",
+  },
+  {
+    id: 6,
+    name: "Jobs",
+    link: "/",
+    icon: "job.svg",
+  },
+  {
+    id: 7,
+    name: "Communities",
+    link: "/",
+    icon: "community.svg",
+  },
+  {
+    id: 8,
+    name: "Premium",
+    link: "/",
+    icon: "logo.svg",
+  },
+  {
+    id: 9,
+    name: "Profile",
+    link: "/",
+    icon: "profile.svg",
+  },
+  {
+    id: 10,
+    name: "More",
+    link: "/",
+    icon: "more.svg",
+  },
+];
+
+const LeftBar = () => {
+  return (
+    <div className="sticky top-0 flex h-screen flex-col justify-between pb-8 pt-2">
+      {/** Logo MENU butto */}
+      <div className="flex flex-col items-center gap-4 text-lg xxl:items-start">
+        {/** LOGO */}
+        <Link href="/" className="rounded-full p-2 hover:bg-[#181818]">
+          <Image src="icons/logo.svg" alt="logo" width={24} height={24} />
+        </Link>
+        <div className="flex flex-col gap-4">
+          {menuList.map((item) => (
+            <Link
+              key={item.id}
+              href={item.link}
+              className="flex items-center gap-4 rounded-full p-2 hover:bg-[#181818]"
+            >
+              <Image src={`icons/${item.icon}`} alt={item.name} width={24} height={24} />
+              <span className="hidden xxl:inline">{item.name}</span>
+            </Link>
+          ))}
+        </div>
+        {/** BUTTON */}
+        <Link
+          href="/"
+          className="flex size-12 items-center justify-center rounded-full bg-white text-black xxl:hidden"
+        >
+          <Image src="icons/post.svg" alt="new post" width={24} height={24} />
+        </Link>
+        <Link
+          href="/"
+          className="hidden rounded-full bg-white px-20 py-2 font-bold text-black xxl:block"
+        >
+          Post
+        </Link>
+      </div>
+      {/* USER */}
+      <div>User</div>
+    </div>
+  );
+};
+
+export default LeftBar;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import LeftBar from "./components/LeftBar";
+import RightBar from "./components/RightBar";
 import "./globals.css";
 
 export default function RootLayout({
@@ -7,7 +9,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="mx-auto flex max-w-screen-md justify-between lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-2xl">
+          <div className="h-screen px-2 xsm:px-4 xxl:px-8">
+            <LeftBar />
+          </div>
+          <div className="border-borderGray h-screen flex-1 border-x-[1px] lg:min-w-[600px]">
+            {children}
+          </div>
+          <div className="ml-4 hidden h-screen flex-1 md:ml-8 lg:flex">
+            <RightBar />
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
