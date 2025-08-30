c20c47a8748c751c60a6a87bec4839b04eaa8b45
samy884 <ssgill884@gmail.com>
2025-08-30 11:31:09 +0530

    creating the initial layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
-import Image from "./components/Image";
+import Link from "next/link";
+import Feed from "../components/Feed";
+import Share from "../components/Share";
 
 const Homepage = () => {
   return (
     <div className="">
-      <div className="relative h-[600px] w-[680px]">
-        <Image src="/post.jpeg" w={600} h={600} tr alt="Picture of the author" />
+      <div className="flex justify-between border-b-[1px] border-borderGray px-4 pt-4 font-bold text-textGray">
+        <Link className="flex items-center border-b-4 border-iconBlue pb-3" href="/">
+          For you
+        </Link>
+        <Link className="flex items-center pb-3" href="/">
+          Following
+        </Link>
+        <Link className="hidden items-center pb-3 md:flex" href="/">
+          React.js
+        </Link>
+        <Link className="hidden items-center pb-3 md:flex" href="/">
+          Javascript
+        </Link>
+        <Link className="hidden items-center pb-3 md:flex" href="/">
+          CSS
+        </Link>
       </div>
+      <Share />
+      <Feed />
     </div>
   );
 };
