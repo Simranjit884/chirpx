8fe1aa398477cabf1974d6e94c85908a7fdb27c6
samy884 <ssgill884@gmail.com>
2025-08-31 16:36:27 +0530

    feed component implementation

---
+++ src/components/PostInfo.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Image from "./Image";
+
+const PostInfo = () => {
+  return (
+    <div className="relative size-4 cursor-pointer">
+      <Image src="icons/infoMore.svg" alt="" w={16} h={16} />
+    </div>
+  );
+};
+
+export default PostInfo;
