c50cc621711dc7bf914f2b9b42ab73ee50a93d62
samy884 <ssgill884@gmail.com>
2025-09-03 19:27:41 +0530

    added username route page

--- src/components/PopularTags.tsx
+++ src/components/PopularTags.tsx
@@ -1,7 +1,61 @@
-import React from "react";
+import Link from "next/link";
+import Image from "./Image";
 
 const PopularTags = () => {
-  return <div>PopularTags</div>;
+  return (
+    <div className="flex flex-col gap-4 rounded-2xl border-[1px] border-borderGray p-4">
+      <h1 className="text-xl font-bold text-textGrayLight">{"What's"} Happening</h1>
+      {/* TREND EVENT */}
+      <div className="flex gap-4">
+        <div className="relative h-20 w-20 overflow-hidden rounded-xl">
+          <Image src="general/post.jpeg" alt="event" w={120} h={120} tr={true} />
+        </div>
+        <div className="flex-1">
+          <h2 className="font-bold text-textGrayLight">Nadal v Federer Grand Slam</h2>
+          <span className="text-sm text-textGray">Last Night</span>
+        </div>
+      </div>
+      {/* TOPICS */}
+      <div className="">
+        <div className="flex items-center justify-between">
+          <span className="text-sm text-textGray">Technology • Trending</span>
+          <Image src="icons/infoMore.svg" alt="info" w={16} h={16} />
+        </div>
+        <h2 className="font-bold text-textGrayLight">OpenAI</h2>
+        <span className="text-sm text-textGray">20K posts</span>
+      </div>
+      {/* TOPICS */}
+      <div className="">
+        <div className="flex items-center justify-between">
+          <span className="text-sm text-textGray">Technology • Trending</span>
+          <Image src="icons/infoMore.svg" alt="info" w={16} h={16} />
+        </div>
+        <h2 className="font-bold text-textGrayLight">OpenAI</h2>
+        <span className="text-sm text-textGray">20K posts</span>
+      </div>
+      {/* TOPICS */}
+      <div className="">
+        <div className="flex items-center justify-between">
+          <span className="text-sm text-textGray">Technology • Trending</span>
+          <Image src="icons/infoMore.svg" alt="info" w={16} h={16} />
+        </div>
+        <h2 className="font-bold text-textGrayLight">OpenAI</h2>
+        <span className="text-sm text-textGray">20K posts</span>
+      </div>
+      {/* TOPICS */}
+      <div className="">
+        <div className="flex items-center justify-between">
+          <span className="text-sm text-textGray">Technology • Trending</span>
+          <Image src="icons/infoMore.svg" alt="info" w={16} h={16} />
+        </div>
+        <h2 className="font-bold text-textGrayLight">OpenAI</h2>
+        <span className="text-sm text-textGray">20K posts</span>
+      </div>
+      <Link href="/" className="text-iconBlue">
+        Show More
+      </Link>
+    </div>
+  );
 };
 
 export default PopularTags;
--- src/components/Recommendations.tsx
+++ src/components/Recommendations.tsx
@@ -1,7 +1,57 @@
-import React from "react";
+import Link from "next/link";
+import Image from "./Image";
 
 const Recommendations = () => {
-  return <div>Recommendations</div>;
+  return (
+    <div className="flex flex-col gap-4 rounded-2xl border-[1px] border-borderGray p-4">
+      {/* USER CARD */}
+      <div className="flex items-center justify-between">
+        {/* IMAGE AND USER INFO */}
+        <div className="flex items-center gap-2">
+          <div className="relative h-10 w-10 overflow-hidden rounded-full">
+            <Image src="general/avatar.png" alt="John Doe" w={100} h={100} tr={true} />
+          </div>
+          <div className="">
+            <h1 className="text-md font-bold">John Doe</h1>
+            <span className="text-sm text-textGray">@johnDoe</span>
+          </div>
+        </div>
+        {/* BUTTON */}
+        <button className="rounded-full bg-white px-4 py-1 font-semibold text-black">Follow</button>
+      </div>
+      <div className="flex items-center justify-between">
+        {/* IMAGE AND USER INFO */}
+        <div className="flex items-center gap-2">
+          <div className="relative h-10 w-10 overflow-hidden rounded-full">
+            <Image src="general/avatar.png" alt="John Doe" w={100} h={100} tr={true} />
+          </div>
+          <div className="">
+            <h1 className="text-md font-bold">John Doe</h1>
+            <span className="text-sm text-textGray">@johnDoe</span>
+          </div>
+        </div>
+        {/* BUTTON */}
+        <button className="rounded-full bg-white px-4 py-1 font-semibold text-black">Follow</button>
+      </div>
+      <div className="flex items-center justify-between">
+        {/* IMAGE AND USER INFO */}
+        <div className="flex items-center gap-2">
+          <div className="relative h-10 w-10 overflow-hidden rounded-full">
+            <Image src="general/avatar.png" alt="John Doe" w={100} h={100} tr={true} />
+          </div>
+          <div className="">
+            <h1 className="text-md font-bold">John Doe</h1>
+            <span className="text-sm text-textGray">@johnDoe</span>
+          </div>
+        </div>
+        {/* BUTTON */}
+        <button className="rounded-full bg-white px-4 py-1 font-semibold text-black">Follow</button>
+      </div>
+      <Link href="/" className="text-iconBlue">
+        Show More
+      </Link>
+    </div>
+  );
 };
 
 export default Recommendations;
